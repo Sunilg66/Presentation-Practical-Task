3cc49a045c9b65cbefe3cd77f13d4fc2e33fad4d
Sunilg66 <sunilkg251@gmail.com>
2021-07-28 20:47:26 +0530

    adding files

---
+++ ReplaceCharacter.java
@@ -0,0 +1,18 @@
+class ReplaceCharacter{
+	
+	public static void main(String[] args){
+		
+		String str= " Karnataka ws a state";
+		
+		String replacechar = str.replace('w','i');
+		System.out.println(replacechar);
+
+	
+	
+	}
+
+
+
+
+
+}
